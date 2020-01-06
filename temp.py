6a7cb4969bad284589c18f828e554c42d520ed59
aliwo <aliwo@naver.com>
2020-01-06 12:49:55 +0900

    PIL 연습

--- temp.py
+++ temp.py
@@ -1,3 +1,17 @@
-import PIL.image
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+i = 0
+for X in X_train:
+    plt.figure(i)
+    plt.imshow(X, cmap='gray')
+    i += 1
+    if i % 4 == 0:
+        break
+
+plt.show()
+
 
 
